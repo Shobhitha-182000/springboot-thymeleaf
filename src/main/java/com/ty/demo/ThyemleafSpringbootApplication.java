f6e6dbdc9065bdc436788493867e48a3b1b75664
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-11-10 10:28:48 +0530

    first commit

---
+++ src/main/java/com/ty/demo/ThyemleafSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyemleafSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyemleafSpringbootApplication.class, args);
+	}
+
+}
